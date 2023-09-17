d3f4eebb10529e38da7250c7204a3c92195957d6
Naitik Patel <naitik2002patel@gmail.com>
2023-09-17 15:39:17 +0530

    deploy: backend

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -29,7 +29,7 @@ function Signin() {
 
   const handlesubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/auth/login", {
+    const response = await fetch("https://profolio-backend-7ugb.onrender.com/api/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
